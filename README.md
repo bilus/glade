782284bfc1bdfc337718416b4ea36e9c69cce9c1
Marcin Bilski <gyamtso@gmail.com>
2025-02-05 11:23:27 +0100

    Add app for turning elm source code into ast.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # glade
 
+TODO: Description, usage as cli, usage as a library.
+
 [![Package Version](https://img.shields.io/hexpm/v/glade)](https://hex.pm/packages/glade)
 [![Hex Docs](https://img.shields.io/badge/hex-docs-ffaff3)](https://hexdocs.pm/glade/)
 
@@ -22,3 +24,29 @@ Further documentation can be found at <https://hexdocs.pm/glade>.
 gleam run   # Run the project
 gleam test  # Run the tests
 ```
+
+### Running elm-syntax
+
+There is a web app you can use to turn Elm source code into AST compatible with
+[elm-syntax](https://package.elm-lang.org/packages/stil4m/elm-syntax/7.3.8/)
+which is an inspiration for Elm AST in this project. The generated JSON isn't
+prefect but it's close enough to help figuring out some quirkier aspects.
+
+``` sh
+cd tools/elm-syntax
+elm reactor
+```
+
+### Contributing: supporting more syntax
+
+The flow for adding support for more Elm syntax is as follows:
+
+1. Add a test case to `test/transpile` by adding two files, `xyz.elm` containing
+   the input and `xyz.expected`.
+2. If necessary, use `tools/elm-syntax` to inspect the corresponding elm-syntax
+   AST.
+3. If you need to add new tokens, add them in `elm/lexer.gleam`. Don't forget
+   about tests.
+4. Implement the parser in `elm/parser.gleam`, adding tests where necessary.
+5. Update the transpiler in `transpile.gleam`.
+6. Run tests via `gleam test`. Iterate until they pass.
---
+++ tools/elm-syntax/src/Main.elm
@@ -0,0 +1,99 @@
+module Main exposing(..)
+
+import Browser
+import Elm.Parser
+import Html exposing (Html, pre, div, text, textarea)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Elm.Syntax.File as File
+import Json.Encode as Encode
+import Json.Decode
+import Json.Print exposing (prettyString)
+
+type alias Model = { src : String }
+
+parse : String -> String
+parse input =
+    case Elm.Parser.parseToFile input of
+        Err e ->
+            "Failed: " ++ Debug.toString e
+
+        Ok v ->
+            "Success: " ++ (v |> File.encode |> valueToString |> prettyPrint)
+
+valueToString : Json.Decode.Value -> String
+valueToString value =
+    Encode.encode 0 value
+
+prettyPrint : String -> String
+prettyPrint js =
+    case js |> prettyString {indent = 2, columns = 100} of
+      Err e -> "Error: " ++ Debug.toString e
+      Ok s -> s
+
+--- INIT
+
+init : Model
+init =
+    { src = "module Foo exposing (foo)\n" }
+
+-- UPDATE
+
+type Msg
+    = Change String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change src ->
+            { model | src = src }
+
+-- VIEW
+
+view : Model -> Html Msg
+view {src} =
+    div [ style "display" "flex"
+        , style "height" "100vh"
+        , style "width" "100%"
+        , style "box-sizing" "border-box"
+        , style "margin" "0"
+        , style "overflow" "hidden"
+    ]
+    [ div [ style "width" "50%"
+          , style "height" "100vh"
+          , style "border-right" "1px solid #ccc"
+          , style "box-sizing" "border-box"
+        ]
+        [ textarea [ placeholder "Elm code"
+                  , value src
+                  , onInput Change
+                  , style "width" "100%"
+                  , style "height" "100%"
+                  , style "resize" "none"
+                  , style "font-family" "monospace"
+                  , style "padding" "0.5rem"
+                  , style "box-sizing" "border-box"
+                  , style "border" "none"
+                  , style "margin" "0"
+                  ] []
+        ]
+    , div [ style "width" "50%"
+          , style "height" "100vh"
+          , style "box-sizing" "border-box"
+        ]
+        [ pre [ style "width" "100%"
+              , style "height" "100%"
+              , style "margin" "0"
+              , style "padding" "0.5rem"
+              , style "font-family" "monospace"
+              , style "white-space" "pre-wrap"
+              , style "overflow" "auto"
+              , style "box-sizing" "border-box"
+              ]
+              [ Html.text (parse src) ]
+        ]
+    ]
+
+main : Program () Model Msg
+main =
+  Browser.sandbox { init = init, update = update, view = view }
